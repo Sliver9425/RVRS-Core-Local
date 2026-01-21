f953875fae8dc9db142f8936a8663f92577e45a5
Damian Quezada <damianalejandro.9422@gmail.com>
2026-01-21 17:09:13 -0500

    fix: update aws credentials and terraform backend syntax

--- infraestructure/backend.tf
+++ infraestructure/backend.tf
@@ -1,12 +1,18 @@
 terraform {
   backend "s3" {
-    bucket                      = "nombre-de-tu-bucket"
-    key                         = "terraform/state/rvrs-stack.tfstate"
-    region                      = "us-east-1"
-    endpoint                    = "s3.us-east-005.backblazeb2.com" # Verifica tu endpoint en B2
+    bucket = "despliegue-RVRS"
+    key    = "terraform.tfstate"
+    region = "us-east-1" # Se deja us-east-1 por compatibilidad, aunque sea B2
+
+    # Cambia 'endpoint' por 'endpoints' y añade https://
+    endpoints = {
+      s3 = "https://s3.us-east-005.backblazeb2.com"
+    }
+
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
-    force_path_style            = true
+    skip_requesting_account_id  = true # Esto ayuda con los errores de permisos de cuenta
+    use_path_style             = true
   }
 }
